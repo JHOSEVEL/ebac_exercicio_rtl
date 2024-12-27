53157cb5eea8cb0778487eb6ab05fb940fbeda8c
josevel silva <josevelmma5@gmail.com>
2024-12-27 17:39:30 -0300

    ok

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className={styles.app}>
       <Post imageUrl="https://www.orangeboxminiaturas.com.br/img/products/batmovel-1989-figura-batman-em-metal-jada-toys-1-24-jad-98260_1_1000.jpg">
-        Olha só que legal minha miniatura do Batmóvel.
+        meu filme favorito e o batiman.
       </Post>
     </div>
   );
--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -7,4 +7,31 @@ describe('Teste para o componente PostComment', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
+
+    it('Deve adicionar dois comentários', () => {
+        render(<PostComment/>);
+
+        // adiciona o primeiro comentário
+        fireEvent.change(screen.getByTestId('comment-textarea'), {
+            target: {
+                value: 'este e uma comentario',
+            }
+        });
+        fireEvent.click(screen.getByTestId('comment-button'));
+    
+        // adiciona o segundo comentário
+        fireEvent.change(screen.getByTestId('comment-textarea'), {
+            target: {
+                value: 'Segundo comentário teste1',
+            }
+        });
+        fireEvent.click(screen.getByTestId('comment-button'));
+
+        expect(screen.getAllByTestId('comment-element')).toHaveLength(2);
+    });
 });
+
+
+
+
+
