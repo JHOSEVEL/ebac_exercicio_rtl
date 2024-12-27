fb83d7a7af157c18bb20a88fc28becc3a86b4d42
josevel silva <josevelmma5@gmail.com>
2024-12-27 17:53:55 -0300

    nudanças ok

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import styles from './App.module.css';
 function App() {
   return (
     <div className={styles.app}>
-      <Post imageUrl="https://www.orangeboxminiaturas.com.br/img/products/batmovel-1989-figura-batman-em-metal-jada-toys-1-24-jad-98260_1_1000.jpg">
+      <Post imageUrl="https://curiosamente.diariodepernambuco.com.br/wp-content/uploads/2016/03/batm%C3%B3vel.jpg">
         meu filme favorito e o batiman.
       </Post>
     </div>
@@ -13,3 +13,5 @@ function App() {
 }
 
 export default App;
+
+
